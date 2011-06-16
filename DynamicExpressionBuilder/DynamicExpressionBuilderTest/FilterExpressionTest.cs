ce0fd29134b6b33a173caee98a321233374b9854
Alberto Monteiro <betimbrasil@gmail.com>
2011-06-15 23:11:30 -0300

    adding new tests

--- DynamicExpressionBuilder/DynamicExpressionBuilderTest/FilterExpressionTest.cs
+++ DynamicExpressionBuilder/DynamicExpressionBuilderTest/FilterExpressionTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using DynamicExpressionBuilder;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -14,31 +15,62 @@ namespace DynamicExpressionBuilderTest
         [TestInitialize]
         public void StartTest()
         {
-            peoples = new List<Person>
-                          {
-                              new Person("Alberto", 21, true),
-                              new Person("Fabio", 24, false),
-                              new Person("Abraão", 23, false),
-                              new Person("Pedro", 26, false)
-                          };
+            peoples = new List<Person>();
+            for (long i = 0; i < 1000000; i++)
+            {
+                peoples.Add(new Person(Guid.NewGuid().ToString(), (int)i, i % 2 == 0));
+            }
         }
 
         [TestMethod]
-        public void ResultExpressionTestHelper()
+        public void ReturnSameValueWhenUseExpressionAnd()
         {
             var target = new FilterExpression<Person>();
 
             target.Start(p => p.Name.Contains("o")).And(p => p.Age <= 25);
 
             var persons = peoples.Where(target.ResultExpression);
+
             var expected = peoples.Where(p => p.Name.Contains("o") && p.Age <= 25);
+
+            bool result = AreCollectionsEquals(persons, expected);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ReturnSameValueWhenUseExpressionOr()
+        {
+            var target = new FilterExpression<Person>();
+
+            target.Start(p => p.Name.Contains("o")).Or(p => p.Age <= 25);
+
+            var persons = peoples.Where(target.ResultExpression);
+
+            var expected = peoples.Where(p => p.Name.Contains("o") || p.Age <= 25);
+
+            bool result = AreCollectionsEquals(persons, expected);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ShouldBeEqualsWithChainedWheres()
+        {
+            var target = new FilterExpression<Person>();
+
+            target.Start(p => p.Name.Contains("o")).And(p => p.Age <= 25);
+
+            var persons = peoples.Where(target.ResultExpression);
+
+            var expected = peoples.Where(p => p.Name.Contains("o"));
+            expected = expected.Where(p => p.Age <= 25);
+
             bool result = AreCollectionsEquals(persons, expected);
             Assert.IsTrue(result);
         }
 
         private bool AreCollectionsEquals(IEnumerable<Person> persons, IEnumerable<Person> expected)
         {
-            if(persons.Count() != expected.Count()) return false;
+            if (persons.Count() != expected.Count()) return false;
             for (int i = 0; i < persons.Count(); i++)
             {
                 if (!persons.ElementAt(i).Equals(expected.ElementAt(i)))
